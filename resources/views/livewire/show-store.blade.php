c8ce126fb0a46116a12e96f8ad5c1bcd3a15f365
brucemak <mtctonyhkhk2010@gmail.com>
2024-05-14 15:58:03 +0800

    update store page

--- resources/views/livewire/show-store.blade.php
+++ resources/views/livewire/show-store.blade.php
@@ -23,25 +23,43 @@
 
     <div class="card bg-base-100 mb-3 shadow-xl">
         <div class="card-body">
-            <p>{{ $store->address }}</p>
+            <a target="_blank" href="https://www.google.com/maps/search/?api=1&query={{ $store->location->latitude }},{{ $store->location->longitude }}">
+                <p><x-icon name="o-map" /> {{ $store->address }}</p>
+            </a>
         </div>
     </div>
-    <div class="card bg-base-100 shadow-xl">
-        <div class="card-body">
-            <p>下一組顧客</p>
-            <div class="flex gap-3">
-                @if($store->status === \App\Enums\StoreStatus::Open)
-                    @forelse($store->store_queue as $number)
-                        <x-badge :value="$number" class="badge-neutral" />
-                    @empty
-                        <x-badge value="無人排隊" class="badge-accent" />
-                    @endforelse
+    <x-card title="下一組顧客" class="mb-3" shadow separator>
+        <div class="flex gap-3">
+            @if($store->status === \App\Enums\StoreStatus::Open)
+                @forelse($store->store_queue as $number)
+                    <x-badge :value="$number" class="badge-neutral" />
+                @empty
+                    <x-badge value="無人排隊" class="badge-accent" />
+                @endforelse
+            @else
+                <x-badge value="檔已收" class="badge-accent" />
+            @endif
+        </div>
+    </x-card>
+
+    @if($store->status === \App\Enums\StoreStatus::Open)
+        <div class="flex gap-3">
+            <x-card title="輪候人數" class="basis-1/2" shadow separator>
+                @if($store->wait_group > 0)
+                    {{ $store->wait_group }}
                 @else
-                    <x-badge value="檔已收" class="badge-accent" />
+                    無人排隊
                 @endif
-            </div>
+            </x-card>
+            <x-card title="輪候時間" class="basis-1/2" shadow separator>
+                @if($store->wait_time > 0)
+                    {{ $store->wait_time }} 分鐘
+                @else
+                    無人排隊
+                @endif
+            </x-card>
         </div>
-    </div>
+    @endif
 </div>
 
 @script
